d205a5b96e13dc347d1cf93cad5ae631a0311ac3
VietSon2002 <97727666+VietSon2002@users.noreply.github.com>
2022-02-18 14:20:43 +0700

    Update and rename BT.java to BT1.java

--- BT1.java
+++ BT1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
-public class  BT {
+public class  BT1 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     byte a = sc.nextByte(), b = sc.nextByte();
