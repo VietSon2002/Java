ea37439ff78dad6b2519efefab9266ee9be5a622
VietSon2002 <97727666+VietSon2002@users.noreply.github.com>
2022-02-18 14:25:02 +0700

    Update and rename BT.java to BT2.java

--- BT2.java
+++ BT2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
-public class  BT {
+public class  BT2 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt(), b = sc.nextInt();
