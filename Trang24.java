0c17f57dc2e8cf990b81444e6670444aafacbc36
VietSon2002 <97727666+VietSon2002@users.noreply.github.com>
2022-02-25 15:18:01 +0700

    Add files via upload

--- Trang24.java
+++ Trang24.java
@@ -1,10 +1,20 @@
+import java.util.Random;
 import java.util.Scanner;
 public class Trang24 {
     public static void main(String[] args) {
+        Random r = new Random();
+        int rd1 = r.nextInt(20)+1;
+        int rd2 = r.nextInt(20)+ rd1;
+        int t = rd2 -rd1;
+        System.out.println("What is "+rd2+"-"+rd1);
         Scanner sc = new Scanner(System.in);
-        int i;
-        for (i = 0 ; i < 2; i++) {
-            System.out.println("Welcome to Java!");
+        try {
+            int a = sc.nextInt();
+            if (a == t ) {
+                System.out.println("Correct");
+            } else System.out.println("In Corret");  
+        } catch (Exception e) {
+            System.out.println("Nhap sai du lieu !");
         }
     }
 }
