97fc8cd13cb0c5fcc07ece510fc970d37b553b0f
VietSon2002 <97727666+VietSon2002@users.noreply.github.com>
2022-02-25 15:01:16 +0700

    Add files via upload

---
+++ Trang24.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Trang24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        for (i = 0 ; i < 2; i++) {
+            System.out.println("Welcome to Java!");
+        }
+    }
+}
